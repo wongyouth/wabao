aa546f84080cc8272e967fe893509c72729fe07c
Yongzhi Wang <wongyouth@gmail.com>
2012-10-09 05:28:25 +0000

    add items#create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,4 +9,13 @@ class ItemsController < ApplicationController
     @items = @q.result.page(params[:page])
     render :index2
   end
+
+  def create
+    item = Item.new params(:item)
+    if item.save
+      render :head => :ok
+    else
+      render :text => 'error'
+    end
+  end
 end
